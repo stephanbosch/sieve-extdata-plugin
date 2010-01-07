a3819d73023ec22dbc26561af63affa2acc43b57
Stephan Bosch <stephan@rename-it.nl>
2010-01-07 12:08:23 +0100

    Removed spurious file.

--- sieve-extdata-config.h
+++
@@ -1,7 +0,0 @@
-/* sieve-extdata-config.h.  Generated from sieve-extdata-config.h.in by configure.  */
-
-/* Define to the full name of this Sieve implementation. */
-/* #undef DICT_LOOKUP_NAME */
-
-/* Define to the version of this Sieve implementation. */
-/* #undef DICT_LOOKUP_VERSION */
