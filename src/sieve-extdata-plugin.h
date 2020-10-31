027b6638de937a34a55abe4997a60d84072e7f83
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-11-01 00:54:59 +0100

    Reformat sieve-extdata-plugin.h.

--- src/sieve-extdata-plugin.h
+++ src/sieve-extdata-plugin.h
@@ -1,7 +1,3 @@
-/* Copyright (c) 2002-2014 Sieve Extdata plugin authors, see the included
-   COPYING file
- */
-
 #ifndef __SIEVE_EXTDATA_PLUGIN_H
 #define __SIEVE_EXTDATA_PLUGIN_H
 
@@ -19,4 +15,4 @@ void sieve_extdata_plugin_unload(struct sieve_instance *svinst, void *context);
 void sieve_extdata_plugin_init(void);
 void sieve_extdata_plugin_deinit(void);
 
-#endif /* __SIEVE_EXTDATA_PLUGIN_H */
+#endif
