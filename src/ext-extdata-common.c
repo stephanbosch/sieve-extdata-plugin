37662cf4e6c809b796fe462c6acbc08c74aefca5
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-11-01 00:53:14 +0100

    Reformat ext-extdata-common.c.

--- src/ext-extdata-common.c
+++ src/ext-extdata-common.c
@@ -1,5 +1,5 @@
-	/* Copyright (c) 2002-2014 Sieve Extdata plugin authors, see the included
-   COPYING file 
+/* Copyright (c) 2002-2014 Sieve Extdata plugin authors, see the included
+   COPYING file
  */
 
 #include "lib.h"
@@ -25,15 +25,15 @@ bool ext_extdata_load(const struct sieve_extension *ext, void **context)
 	struct ext_extdata_context *ext_data;
 	const char *dict_uri;
 
-	if ( *context != NULL ) {
+	if (*context != NULL) {
 		ext_extdata_unload(ext);
 		*context = NULL;
 	}
 
 	dict_uri = sieve_setting_get(ext->svinst, "sieve_extdata_dict_uri");
-	if ( dict_uri == NULL ) {
-		sieve_sys_warning(ext->svinst, 
-			"extdata: no dict uri specified, extension is unconfigured "
+	if  (dict_uri == NULL) {
+		sieve_sys_warning(ext->svinst, "extdata: "
+			"no dict uri specified, extension is unconfigured "
 			"(sieve_extdata_dict_uri is not set).");
 	}
 
@@ -41,17 +41,16 @@ bool ext_extdata_load(const struct sieve_extension *ext, void **context)
 	ext_data->dict_uri = i_strdup(dict_uri);
 	ext_data->var_ext = sieve_ext_variables_get_extension(ext->svinst);
 
-	*context = (void *) ext_data;
-
+	*context = (void *)ext_data;
 	return TRUE;
 }
 
 void ext_extdata_unload(const struct sieve_extension *ext)
 {
 	struct ext_extdata_context *ext_data =
-		(struct ext_extdata_context *) ext->context;
+		(struct ext_extdata_context *)ext->context;
 
-	if ( ext_data == NULL )
+	if (ext_data == NULL)
 		return;
 
 	i_free(ext_data->dict_uri);
@@ -66,12 +65,13 @@ struct ext_extdata_interpreter_context {
 	struct dict *dict;
 };
 
-static void ext_extdata_interpreter_free
-(const struct sieve_extension *ext ATTR_UNUSED,
-	struct sieve_interpreter *interp ATTR_UNUSED, void *context)
+static void
+ext_extdata_interpreter_free(const struct sieve_extension *ext ATTR_UNUSED,
+			     struct sieve_interpreter *interp ATTR_UNUSED,
+			     void *context)
 {
-	struct ext_extdata_interpreter_context *ictx = 
-		(struct ext_extdata_interpreter_context *) context;
+	struct ext_extdata_interpreter_context *ictx =
+		(struct ext_extdata_interpreter_context *)context;
 
 	if (ictx->dict != NULL)
 		dict_deinit(&ictx->dict);
@@ -89,8 +89,8 @@ ext_extdata_interpreter_get_context
 {
 	struct sieve_instance *svinst = ext->svinst;
 	struct ext_extdata_context *ext_data =
-		(struct ext_extdata_context *) ext->context;
-	struct ext_extdata_interpreter_context *ictx = 
+		(struct ext_extdata_context *)ext->context;
+	struct ext_extdata_interpreter_context *ictx =
 		(struct ext_extdata_interpreter_context *)
 		sieve_interpreter_extension_get_context(renv->interp, ext);
 	const char *error;
@@ -100,13 +100,14 @@ ext_extdata_interpreter_get_context
 	int ret;
 
 	/* If there is already an interpreter context, return it rightaway */
-	if ( ictx != NULL )
+	if (ictx != NULL)
 		return ictx;
 
-	/* We cannot access the dict if no URI is configured or when the username is
-	 * not known.
+	/* We cannot access the dict if no URI is configured or when the
+	   username is not known.
 	 */
-	if ( ext_data == NULL || ext_data->dict_uri == NULL || svinst->username == NULL )
+	if (ext_data == NULL || ext_data->dict_uri == NULL ||
+	    svinst->username == NULL)
 		return NULL;
 
 	/* Initialize the dict */
@@ -114,11 +115,13 @@ ext_extdata_interpreter_get_context
 	dict_set.username = svinst->username;
 	dict_set.base_dir = svinst->base_dir;
 	ret = dict_init(ext_data->dict_uri, &dict_set, &dict, &error);
-	if ( ret < 0 ) {
+	if (ret < 0) {
 		sieve_runtime_critical(renv, NULL,
 			"failed to initialize vnd.dovecot.extdata extension",
-			"sieve dict backend: failed to initialize dict with data `%s' "
-			"for user `%s': %s", ext_data->dict_uri, svinst->username, error);
+			"sieve dict backend: "
+			"failed to initialize dict with data `%s' "
+			"for user `%s': %s",
+			ext_data->dict_uri, svinst->username, error);
 	}
 
 	/* Create interpreter context */
@@ -127,11 +130,11 @@ ext_extdata_interpreter_get_context
 	ictx->dict = dict;
 
 	/* Register interpreter extension to deinit the dict when the interpreter
-	 * is freed.
+	   is freed.
 	 */
-	sieve_interpreter_extension_register
-		(renv->interp, ext, &extdata_interpreter_extension, (void *) ictx);
-
+	sieve_interpreter_extension_register(renv->interp, ext,
+					     &extdata_interpreter_extension,
+					     (void *)ictx);
 	return ictx;
 }
 
@@ -139,36 +142,38 @@ ext_extdata_interpreter_get_context
  * Dict lookup
  */
 
-const char *ext_extdata_get_value
-(const struct sieve_runtime_env *renv, const struct sieve_extension *this_ext,
-	const char *identifier)
+const char *
+ext_extdata_get_value(const struct sieve_runtime_env *renv,
+		      const struct sieve_extension *this_ext,
+		      const char *identifier)
 {
-	struct ext_extdata_interpreter_context *ictx = 
+	struct ext_extdata_interpreter_context *ictx =
 		ext_extdata_interpreter_get_context(this_ext, renv);
 	const char *key, *error;
 	const char *value = NULL;
 	int ret;
 
-	if ( ictx == NULL ) {
+	if (ictx == NULL) {
 		sieve_runtime_trace_error(renv, "extension is not configured");
 		return NULL;
 	}
 
-	if ( ictx->dict == NULL ) {
-		sieve_runtime_trace_error(renv, "extension is not properly configured");
+	if (ictx->dict == NULL) {
+		sieve_runtime_trace_error(
+			renv, "extension is not properly configured");
 		return NULL;
 	}
 
 	key = t_strconcat("priv/", identifier, NULL);
-
-	if ( (ret=dict_lookup(ictx->dict,
-		pool_datastack_create(), key, &value, &error)) <= 0 ) {
+	ret = dict_lookup(ictx->dict, pool_datastack_create(), key,
+			  &value, &error);
+	if (ret <= 0) {
 		if (ret < 0)
-			sieve_runtime_warning(renv, NULL,
+			sieve_runtime_warning(
+				renv, NULL,
 				"extdata: failed to lookup value `%s': %s",
 				identifier, error);
 		return NULL;
 	}
-
 	return value;
 }
