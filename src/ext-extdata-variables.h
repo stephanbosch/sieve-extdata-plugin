c7f66f1cf4d7c68b8cb30ffd8da5eccb49ed7be2
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-11-01 00:53:57 +0100

    Reformat ext-extdata-variables.h.

--- src/ext-extdata-variables.h
+++ src/ext-extdata-variables.h
@@ -1,13 +1,9 @@
-/* Copyright (c) 2002-2014 Sieve Extdata plugin authors, see the included
-   COPYING file 
- */
-
 #ifndef __EXT_EXTDATA_VARIABLES_H
 #define __EXT_EXTDATA_VARIABLES_H
 
-void ext_extdata_variables_init
-	(const struct sieve_extension *this_ext, struct sieve_validator *valdtr);
+void ext_extdata_variables_init(const struct sieve_extension *this_ext,
+				struct sieve_validator *valdtr);
 
-#endif /* __EXT_EXTDATA_VARIABLES_H */
+#endif
 
 
